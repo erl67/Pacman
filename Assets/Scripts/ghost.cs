792a44fb7d9ffaafa3ffe623743b13f8f7bddc6e
erl67 <erl67@pitt.edu>
2018-02-21 12:51:57 -0500

    init

---
+++ Assets/Scripts/ghost.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghost : MonoBehaviour {
+
+    public GameObject body;
+    public GameObject mouth;
+    public GameObject head;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
